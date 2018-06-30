4e357fc1c88220982026ce8af63ab4a8cb3f12b3
Horpey <adeniran1059@gmail.com>
2018-06-30 11:41:17 +0100

    Default Currency Preview

--- js/app.js
+++ js/app.js
@@ -3,20 +3,23 @@ const insertHTML = (e) => {
 
 }
 const getFromCurrencyName = () => {
-  return el("#fromCurrency").value;
+    return el("#fromCurrency").value;
 };
 const getToCurrencyName = () => {
-  return el("#toCurrency").value;
+    return el("#toCurrency").value;
 };
 const getFromCurrencyId = () => {
-  return el("#fromCurrency").value;
+    return el("#fromCurrency").value;
 };
 const getToCurrencyId = () => {
-  return el("#toCurrency").value;
+    return el("#toCurrency").value;
 };
 const getFromCurrencyValue = () => {
-  return el("#fromCurrencyValue").value;
+    return el("#fromCurrencyValue").value;
 };
+
+document.getElementById("viewValue").innerHTML = `00.00`;
+
 
 // sortSelectOptions('#fromCurrency', true);
 
@@ -28,28 +31,28 @@ idSymbolTo = 'Lek';
 
 convert = (s) => {
 
-  let fromValue = document.querySelector('#fromCurrencyValue').value;
-  let fromCurr = document.querySelector('#fromCurrency').value;
-  let toCurr = document.querySelector('#toCurrency').value;
+    let fromValue = document.querySelector('#fromCurrencyValue').value;
+    let fromCurr = document.querySelector('#fromCurrency').value;
+    let toCurr = document.querySelector('#toCurrency').value;
 
-  const query = `${fromCurr}_${toCurr}`;
-  const requestUrl = `https://free.currencyconverterapi.com/api/v5/convert?q=${query}&compact=ultra`;
+    const query = `${fromCurr}_${toCurr}`;
+    const requestUrl = `https://free.currencyconverterapi.com/api/v5/convert?q=${query}&compact=ultra`;
 
-  fetch(requestUrl)
-    .then(response => response.json())
-    .then(responseValue => {
+    fetch(requestUrl)
+        .then(response => response.json())
+        .then(responseValue => {
 
-      let unitValue = responseValue[`${fromCurr}_${toCurr}`];
+            let unitValue = responseValue[`${fromCurr}_${toCurr}`];
 
-      let currencyConverted = fromValue * unitValue;
+            let currencyConverted = fromValue * unitValue;
 
 
-      console.log(requestUrl);
+            console.log(requestUrl);
 
-      document.getElementById("viewValue").innerHTML = `${idSymbolTo} ${Math.round(currencyConverted)}.00`;
+            document.getElementById("viewValue").innerHTML = `${idSymbolTo} ${Math.round(currencyConverted)}.00`;
 
-      document.getElementById("conversionUnit").innerHTML = `${idSymbolFrom} 1 = ${idSymbolTo}  ${Math.round(unitValue)} `;
-    });
+            document.getElementById("conversionUnit").innerHTML = `${idSymbolFrom} 1 = ${idSymbolTo}  ${Math.round(unitValue)} `;
+        });
 
 }
 
@@ -58,175 +61,114 @@ convert = (s) => {
 
 // Service Worker registration
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('./service-worker.js', {
-      scope: './'
-    })
-    .then(registration => {
-      console.log("Service Worker Registered", registration);
-    })
-    .catch(err => {
-      console.log("Service Worker failed to Register", err);
-    })
+    navigator.serviceWorker
+        .register('./service-worker.js', {
+            scope: './'
+        })
+        .then(registration => {
+            console.log("Service Worker Registered", registration);
+        })
+        .catch(err => {
+            console.log("Service Worker failed to Register", err);
+        })
 }
 
 // // Function for API Call
 
 fetch('https://free.currencyconverterapi.com/api/v5/countries')
-  .then(response => response.json())
-  .then(myJson => {
+    .then(response => response.json())
+    .then(myJson => {
 
 
-    let html = '';
-    for (let country of Object.values(myJson.results)) {
-      // console.log(country);
-      html += `<option id="${country.currencySymbol}" value="${country.currencyId}">${country.currencyName}</option>`;
-    }
-    el("#fromCurrency").insertAdjacentHTML('afterbegin', html);
-    el("#toCurrency").insertAdjacentHTML('afterbegin', html);
-  });
+        let html = '';
+        for (let country of Object.values(myJson.results)) {
+            // console.log(country);
+            html += `<option id="${country.currencySymbol}" value="${country.currencyId}">${country.currencyName}</option>`;
+        }
+        el("#fromCurrency").insertAdjacentHTML('afterbegin', html);
+        el("#toCurrency").insertAdjacentHTML('afterbegin', html);
+    });
 
 
 
 
 showFromId = (s) => {
-  idSymbolFrom = s[s.selectedIndex].id;
-  userConnection()
-  convert()
+    idSymbolFrom = s[s.selectedIndex].id;
+    userConnection()
+    convert()
 }
 
 
 showFromIdTwo = (s) => {
-  idSymbolTo = s[s.selectedIndex].id;
-  userConnection()
-  convert()
+    idSymbolTo = s[s.selectedIndex].id;
+    userConnection()
+    convert()
 }
 
 // Toastr Diplay
 userConnection = () => {
-  if (navigator.onLine) {
+    if (navigator.onLine) {
 
-  } else {
-    // Display an info toast with no title
-    toastr.warning(`Sorry Conversion can't be made offline`);
-    toastr.options = {
-      "positionClass": "toast-bottom-center"
+    } else {
+        // Display an info toast with no title
+        toastr.warning(`Sorry Conversion can't be made offline`);
+        toastr.options = {
+            "positionClass": "toast-bottom-center"
+        }
     }
-  }
 }
 
 
 // IndexedDb initialization
 
 const dbPromise = idb.open('currencyConverter', 3, (upgradeDb) => {
-  switch (upgradeDb.oldVersion) {
-    case 0:
-      upgradeDb.createObjectStore('countries', {
-        keyPath: 'currencyId'
-      });
-    case 1:
-      let countriesStore = upgradeDb.transaction.objectStore('countries');
-      countriesStore.createIndex('country', 'currencyName');
-      countriesStore.createIndex('country-code', 'currencyId');
-    case 2:
-      upgradeDb.createObjectStore('conversionRates', {
-        keyPath: 'query'
-      });
-      let ratesStore = upgradeDb.transaction.objectStore('conversionRates');
-      ratesStore.createIndex('rates', 'query');
-  }
+    switch (upgradeDb.oldVersion) {
+        case 0:
+            upgradeDb.createObjectStore('countries', {
+                keyPath: 'currencyId'
+            });
+        case 1:
+            let countriesStore = upgradeDb.transaction.objectStore('countries');
+            countriesStore.createIndex('country', 'currencyName');
+            countriesStore.createIndex('country-code', 'currencyId');
+        case 2:
+            upgradeDb.createObjectStore('conversionRates', {
+                keyPath: 'query'
+            });
+            let ratesStore = upgradeDb.transaction.objectStore('conversionRates');
+            ratesStore.createIndex('rates', 'query');
+    }
 });
 
 
 document.addEventListener('DOMContentLoaded', () => {
-  /*
-   Fetch Countries 
-    */
-  fetch('https://free.currencyconverterapi.com/api/v5/countries')
-    .then(res => res.json())
-    .then(res => {
-      Object.values(res.results).forEach(country => {
-        dbPromise.then(db => {
-          const countries = db.transaction('countries', 'readwrite').objectStore('countries');
-          countries.put(country);
-        })
-      });
-      dbPromise.then(db => {
-        const countries = db.transaction('countries', 'readwrite').objectStore('countries');
-        const countriesIndex = countries.index('country');
-        countriesIndex.getAll().then(currencies => {
-          // fetchCountries(currencies);
-        })
-      })
-    }).catch(() => {
-    dbPromise.then(db => {
-      const countries = db.transaction('countries').objectStore('countries');
-      const countriesIndex = countries.index('country');
-      countriesIndex.getAll().then(currencies => {
-        // fetchCountries(currencies);
-      })
+    /*
+     Fetch Countries 
+      */
+    fetch('https://free.currencyconverterapi.com/api/v5/countries')
+        .then(res => res.json())
+        .then(res => {
+            Object.values(res.results).forEach(country => {
+                dbPromise.then(db => {
+                    const countries = db.transaction('countries', 'readwrite').objectStore('countries');
+                    countries.put(country);
+                })
+            });
+            dbPromise.then(db => {
+                const countries = db.transaction('countries', 'readwrite').objectStore('countries');
+                const countriesIndex = countries.index('country');
+                countriesIndex.getAll().then(currencies => {
+                    // fetchCountries(currencies);
+                })
+            })
+        }).catch(() => {
+            dbPromise.then(db => {
+                const countries = db.transaction('countries').objectStore('countries');
+                const countriesIndex = countries.index('country');
+                countriesIndex.getAll().then(currencies => {
+                    // fetchCountries(currencies);
+                })
 
-    });
-  });
-});
-
-
-// Cache API
-var CACHE_VERSION = 1;
-
-// Shorthand identifier mapped to specific versioned cache.
-var CURRENT_CACHES = {
-  font: 'font-cache-v' + CACHE_VERSION
-};
-
-self.addEventListener('activate', function(event) {
-  var expectedCacheNames = Object.values(CURRENT_CACHES);
-
-  // Active worker won't be treated as activated until promise
-  // resolves successfully.
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (!expectedCacheNames.includes(cacheName)) {
-            console.log('Deleting out of date cache:', cacheName);
-
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
-
-self.addEventListener('fetch', function(event) {
-  console.log('Handling fetch event for', event.request.url);
-
-  event.respondWith(
-
-    // Opens Cache objects that start with 'font'.
-    caches.open(CURRENT_CACHES['font']).then(function(cache) {
-      return cache.match(event.request).then(function(response) {
-        if (response) {
-          console.log('Found response in cache:', response);
-
-          return response;
-        }
-
-        console.log('Fetching request from the network');
-
-        return fetch(event.request).then(function(networkResponse) {
-          cache.put(event.request, networkResponse.clone());
-
-          return networkResponse;
+            });
         });
-      }).catch(function(error) {
-
-        // Handles exceptions that arise from match() or fetch().
-        console.error('Error in fetch handler:', error);
-
-        throw error;
-      });
-    })
-  );
 });
--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Set a name for the current cache
-var cacheName = 'v1'; 
+var cacheName = 'v1';
 
 // Default files to always cache
 var cacheFiles = [
@@ -17,31 +17,31 @@ var cacheFiles = [
 ]
 
 
-self.addEventListener('install', function(e) {
-    console.log('[ServiceWorker] Installed');
+self.addEventListener('install', function (e) {
+  console.log('[ServiceWorker] Installed');
 
-    // e.waitUntil Delays the event until the Promise is resolved
-    e.waitUntil(
+  // e.waitUntil Delays the event until the Promise is resolved
+  e.waitUntil(
 
-      // Open the cache
-      caches.open(cacheName).then(function(cache) {
+    // Open the cache
+    caches.open(cacheName).then(function (cache) {
 
-        // Add all the default files to the cache
+      // Add all the default files to the cache
       console.log('[ServiceWorker] Caching cacheFiles');
       return cache.addAll(cacheFiles);
-      })
+    })
   ); // end e.waitUntil
 });
 
 
-self.addEventListener('activate', function(e) {
-    console.log('[ServiceWorker] Activated');
+self.addEventListener('activate', function (e) {
+  console.log('[ServiceWorker] Activated');
 
-    e.waitUntil(
+  e.waitUntil(
 
-      // Get all the cache keys (cacheName)
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(cacheNames.map(function(thisCacheName) {
+    // Get all the cache keys (cacheName)
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(cacheNames.map(function (thisCacheName) {
 
         // If a cached item is saved under a previous cacheName
         if (thisCacheName !== cacheName) {
@@ -57,7 +57,7 @@ self.addEventListener('activate', function(e) {
 });
 
 
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', function (e) {
   console.log('[ServiceWorker] Fetch', e.request.url);
 
   // e.respondWidth Responds to the fetch event
@@ -67,46 +67,46 @@ self.addEventListener('fetch', function(e) {
     caches.match(e.request)
 
 
-      .then(function(response) {
+    .then(function (response) {
 
-        // If the request is in the cache
-        if ( response ) {
-          console.log("[ServiceWorker] Found in Cache", e.request.url, response);
-          // Return the cached version
-          return response;
-        }
+      // If the request is in the cache
+      if (response) {
+        console.log("[ServiceWorker] Found in Cache", e.request.url, response);
+        // Return the cached version
+        return response;
+      }
 
-        // If the request is NOT in the cache, fetch and cache
+      // If the request is NOT in the cache, fetch and cache
 
-        var requestClone = e.request.clone();
-        return fetch(requestClone)
-          .then(function(response) {
+      var requestClone = e.request.clone();
+      return fetch(requestClone)
+        .then(function (response) {
 
-            if ( !response ) {
-              console.log("[ServiceWorker] No response from fetch ")
-              return response;
-            }
+          if (!response) {
+            console.log("[ServiceWorker] No response from fetch ")
+            return response;
+          }
 
-            var responseClone = response.clone();
+          var responseClone = response.clone();
 
-            //  Open the cache
-            caches.open(cacheName).then(function(cache) {
+          //  Open the cache
+          caches.open(cacheName).then(function (cache) {
 
-              // Put the fetched response in the cache
-              cache.put(e.request, responseClone);
-              console.log('[ServiceWorker] New Data Cached', e.request.url);
+            // Put the fetched response in the cache
+            cache.put(e.request, responseClone);
+            console.log('[ServiceWorker] New Data Cached', e.request.url);
 
-              // Return the response
-              return response;
-      
-                }); // end caches.open
+            // Return the response
+            return response;
 
-          })
-          .catch(function(err) {
-            console.log('[ServiceWorker] Error Fetching & Caching New Data', err);
-          });
+          }); // end caches.open
+
+        })
+        .catch(function (err) {
+          console.log('[ServiceWorker] Error Fetching & Caching New Data', err);
+        });
 
 
-      }) // end caches.match(e.request)
+    }) // end caches.match(e.request)
   ); // end e.respondWith
 });
